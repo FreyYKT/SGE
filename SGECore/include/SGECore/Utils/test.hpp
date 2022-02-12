52252e6a2721ab2a87233e908f2aa1d2b318eb24
FreyYKT <34671196+FreyYKT@users.noreply.github.com>
2022-02-13 00:11:31 +0900

    Delete test files

--- SGECore/include/SGECore/Utils/test.hpp
+++
@@ -1,3 +0,0 @@
-namespace SGE {
-	void debug();
-}
--- SGECore/src/SGECore/Utils/test.cpp
+++
@@ -1,9 +0,0 @@
-#include <iostream>
-
-#include "SGECore/Utils/test.hpp"
-
-namespace SGE {
-	void debug() {
-		std::cout << "SGECore is working!";
-	}
-}
